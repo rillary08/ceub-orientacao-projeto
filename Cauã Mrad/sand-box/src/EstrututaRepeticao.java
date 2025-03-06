5ee7803270ed26f75d4b6fee0e10d7cad35766d0
Cauã Gonçalves Xavier Mrad <caua.xavier@dinfor.uniceub>
2025-03-06 19:39:10 -0300

    inicio aula revisao java

---
+++ Cauã Mrad/sand-box/src/EstrututaRepeticao.java
@@ -0,0 +1,31 @@
+public class EstrututaRepeticao {
+
+    public static void main(String[] args) {
+        
+        //i = i + 1 == i++
+        //for
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("valor: " + i);
+
+        } 
+
+        //proxima aula Lista e for melhor
+
+
+        int contador = 0;
+        while (contador < 5) {
+            System.out.println("contador: "+contador);
+            contador++;
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
