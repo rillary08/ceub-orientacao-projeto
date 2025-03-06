90b5332104e6d169297babb569404e93e3b4101e
Edilson Nogueira de Oliveira Junior <edilson.junior@dinfor.uniceub>
2025-03-06 19:39:15 -0300

    incio da aula revisao jav

---
+++ Edilson 2/sand-box/src/EstruturaRepeticao.java
@@ -0,0 +1,24 @@
+public class EstruturaRepeticao {
+
+public static void main(String[] args) {
+
+    // 1 = 1 + 1 == 1++
+    //for
+for(int i=1; i <= 5; i++){
+System.out.println("Valor: " +i);
+}
+
+
+
+int contador = 0;
+while(contador < 5) {
+    System.out.println("contador" + contador);
+    contador++;
+}
+
+
+
+
+}
+
+}
---
+++ Edilson 2/sand-box/src/VariaveisTipo.java
@@ -0,0 +1,22 @@
+public class VariaveisTipo {
+
+public static void main(String[] args) {
+    
+
+Integer numero = 10;
+Double preco = 25.99;
+char letra = 'A';
+Boolean ativo = true;
+Boolean ehferias = false;
+String texto = "Olá mundo!";
+
+System.out.println(numero);
+System.out.println(preco);
+System.out.println(letra);
+System.out.println(ativo);
+System.out.println(ehferias);
+System.out.println(texto);
+
+}
+
+}
