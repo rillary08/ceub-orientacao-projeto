57c5342f59d5b795fff27cc42302b4e149425fe1
Bruno Guimarães Alves <bruno.galves@dinfor.uniceub>
2025-03-06 19:39:10 -0300

    aula java

---
+++ bruno alves/sand-box/src/EstruturaRepeticao.java
@@ -0,0 +1,19 @@
+public class EstruturaRepeticao {
+
+    public static void main(String[] args) {
+        
+        //for
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("vlor: " + i);
+        }
+
+        // lista?
+
+        //while
+        int contador = 0;
+        while (contador < 5) {
+            System.out.println("contador: " + contador);
+            contador++;
+        }
+    }
+}
