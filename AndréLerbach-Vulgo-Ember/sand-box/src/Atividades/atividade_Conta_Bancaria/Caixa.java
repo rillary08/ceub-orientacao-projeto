7e31a67a2960e07485eeb50ed28b7b9286d31afc
André de Alencar Lerbach <andre.lerbach@dinfor.uniceub>
2025-03-25 21:02:39 -0300

    Fazendo os comentários do código

--- AndréLerbach-Vulgo-Ember/sand-box/src/Atividades/atividade_Conta_Bancaria/Caixa.java
+++ AndréLerbach-Vulgo-Ember/sand-box/src/Atividades/atividade_Conta_Bancaria/Caixa.java
@@ -1,11 +1,19 @@
+// Arquivo Caixa, onde vamos instanciar a classe ContaBancaria
 package atividade_Conta_Bancaria;
 
 public class Caixa {
     public static void main(String[] args) {
+
+        // Declarando as variáveis da classe (instanciando)
         ContaBancaria jarvis = new ContaBancaria("Jarvis", "123.456.789-01" );
 
+        // Chamando os métodos com as especificações
         jarvis.info();
+
+        // Faz o depósito de 200.0 dinheiros
         jarvis.depositar(200.0);
+
+        // Faz o saque de 900.0 dinheiros, como o salto é mais alto do que o saldo, o comando vai devolver "Valor Inválido", ou algo assim
         jarvis.sacar(900.0);
 
     }
--- AndréLerbach-Vulgo-Ember/sand-box/src/Atividades/atividade_Conta_Bancaria/ContaBancaria.java
+++ AndréLerbach-Vulgo-Ember/sand-box/src/Atividades/atividade_Conta_Bancaria/ContaBancaria.java
@@ -1,5 +1,9 @@
+
+// ARQUIVO CHAMANDO ContaBancaria
 package atividade_Conta_Bancaria;
 
+
+    // Declarando as variáveis na classe da Conta Bancária
 public class ContaBancaria {
     Double dinheiro;
     Double saldo;
@@ -7,20 +11,20 @@ public class ContaBancaria {
     String cpf;
 
 
+    // Construtor da Conta Bancária (inserindo as variáveis e fazendo o "link" das variáveis 
+    // que estão entre parentesis do construtor e nas variáveis que estão declaradas na classe)
     public ContaBancaria(String nome, String cpf){
         this.saldo = 500.00;
         this.nome = nome;
         this.cpf = cpf;
     }
 
-    public void saldo(){
-        System.out.println("\n Saldo na conta:"+dinheiro);
-    }
-
+    // Método que adiciona dinheiro na conta
     public void depositar(Double dinheiro){
         System.out.println(saldo+dinheiro);
     }
 
+    // Método que remove dinheiro da conta, considerando saques negativos (usando if/else para definir o que é disponível ou não)
     public void sacar(Double dinheiro){
         if (saldo-dinheiro <= 0){
             System.out.println("Saldo indisponível para saque");
@@ -29,6 +33,7 @@ public class ContaBancaria {
         }
     }
 
+    // Método para mostrar as informações da conta
     public void info() {
         System.out.println("\n Nome: "+nome+
                             "\n CPF: "+cpf+
