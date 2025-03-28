2d583654aeda951642a1752da14418a3534386fd
Edilson Nogueira de Oliveira Junior <edilson.junior@dinfor.uniceub>
2025-03-27 21:48:34 -0300

    Atividade 2

--- Edilson 2/sand-box/Exercícios/Exercícios/Atividade2/Produto.java
+++ Edilson 2/sand-box/Exercícios/Exercícios/Atividade2/Produto.java
@@ -2,4 +2,24 @@ package Atividade2;
 
 public class Produto {
 
+   String nome;
+   Double preco;
+   int quantidade;
+
+   private Produto (String nome, Double preco, int quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+   }
+
+   public void comprar(int qauntidae){
+    System.out.println(quantidade = 1);
+   }
+
+   public void vender(int quantidade){
+    System.out.println(quantidade - 1);
+   }
+   
+   
+
 }
