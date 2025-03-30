4b91858e5f61abcf6898a9a55658ddcef5bd048e
Ember Misaka <embermisaka@gmail.com>
2025-03-30 00:41:26 -0300

    resolvendo um problema

--- AndréLerbach-Vulgo-Ember/sand-box/src/Atividades/Encapsulamento/src/App.java
+++
@@ -1,8 +0,0 @@
-package Atividades.Encapsulamento.src;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Produto refrigerante = new Produto("Refrigerante", 7.65, 100);
-        System.out.println(refrigerante);
-    }
-}
