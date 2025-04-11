f1d966de34c20ba09e2583fdb264c756444a699f
Júlia Barrozo Rodrigues Pereira <julia.barrozo@sempreceub.com>
2025-04-10 21:25:25 -0300

    adicionando novas classes

---
+++ Julia Barrozo Rodrigues/sand-box/src/Atividades/Pokemon/AtividadePokemon/src/Ginasio.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Ginasio {
+    private String nome;
+    private List<Pokemon> PokemonsTreinados;
+    private List<Treinamento> ListaTreinamentos;
+
+    public void imprimirPokemon() {
+        System.out.println();
+    }
+}
---
+++ Julia Barrozo Rodrigues/sand-box/src/Atividades/Pokemon/AtividadePokemon/src/Treinamento.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Treinamento {
+    private String descricao;
+    private int duracao;
+
+
+}
