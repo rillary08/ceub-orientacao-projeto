42c0a61242947c2f7827980ff06d1ee619499d7e
EmberMisaka <embermisaka@gmail.com>
2025-05-14 19:56:06 -0300

    Lançando a Última atividade

--- Andre_Lerbach_Vulgo_Ember/Atividades/Scanner/src/AtividadeScanner.java
+++ Andre_Lerbach_Vulgo_Ember/Atividades/Scanner/src/AtividadeScanner.java
@@ -1,4 +1,3 @@
-
 package Atividades.Scanner.src;
 import java.util.Scanner;
 
@@ -15,16 +14,6 @@ public class AtividadeScanner {
 
         System.out.println("[-----Resultado-----]");
         System.out.println(num1+num2);
-
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Atividade Palíndromo");
-
-        System.out.println("Digite o primeiro número :");
-        int Num1 = scanner.nextInt();
-        System.out.println("Digite o segundo número :");
-        int Num2 = scanner.nextInt();
-        System.out.println("A soma é " + (Num1 + Num2));
 
         scanner.close();
     }
