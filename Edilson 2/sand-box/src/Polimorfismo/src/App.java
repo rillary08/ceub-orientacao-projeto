379c63c5eb7bf3fd475ea5b5f9a17f8173690547
Edilson Nogueira de Oliveira Junior <edilson.junior@dinfor.uniceub>
2025-05-22 20:57:38 -0300

    Aula Polimorfismo

--- Edilson 2/sand-box/src/Polimorfismo/src/App.java
+++ Edilson 2/sand-box/src/Polimorfismo/src/App.java
@@ -1,5 +1,81 @@
+import java.util.List;
+import java.util.function.DoubleUnaryOperator;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        List<Dispositivo> dispositivos = List.of(new Celular(), new RoboAspirador(), new VideoGame());
+    }
+}
+
+abstract class Dispositivo {
+    protected Double porcentagemBateria;
+    public abstract void acessarInternet();
+    public abstract void entrarModoEconomiaEnergia();
+
+    
+}
+
+
+public class  Celular extends Dispositivo {
+
+    public void realizarLigacao(){
+        System.out.println("Realizando ligação telefônica");
+    }
+
+    @Override
+    public void acessarInternet() {
+        System.out.println("Criando conexão 4G");
+       }
+
+    @Override
+    public void entrarModoEconomiaEnergia() {
+        System.out.println("Diminuir brilho da tela.");
+    }
+
+ 
+    }
+
+   
+
+    
+
+
+
+class VideoGame extends Dispositivo {
+
+    public void executarJogo() {
+        System.out.println("Executar Jogo");
+    }
+
+    @Override
+    public void acessarInternet() {
+        System.out.println("Iniciando conexão com RJ45");
+    }
+
+    @Override
+    public void entrarModoEconomiaEnergia() {
+        System.out.println("Delisgar o controle");
+    }
+    
+
+    class RoboAspirador extends Dispositivo {
+
+        
+        public void apirar(){
+            System.out.println("Aspirar");
+        }
+
+        @Override
+        public void acessarInternet() {
+            System.out.println("Iniciar com o WIFI");
+        }
+
+        @Override
+        public void entrarModoEconomiaEnergia() {
+           System.out.println("Retornar pra base");
+        }
+
     }
 }
