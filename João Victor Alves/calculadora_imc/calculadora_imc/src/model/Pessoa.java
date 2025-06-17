b74a2ab49b57a1d7c6c752c6c2134aee81bed4f7
Joao Victor Alves Rodrigues <joao.arodrigues@dinfor.uniceub>
2025-06-17 20:00:58 -0300

    attcalculadora

--- João Victor Alves/calculadora_imc/calculadora_imc/src/model/Pessoa.java
+++ João Victor Alves/calculadora_imc/calculadora_imc/src/model/Pessoa.java
@@ -1,32 +1,37 @@
 package model;
 
-import model.contract.IPessoa;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class Pessoa implements IPessoa {
+public abstract class Pessoa {
+    protected String nome;
+    protected double peso;
+    protected double altura;
+    protected LocalDate data;
 
-    private String nome;
-    private double peso;
-    private double altura;
-
-    public Pessoa(double altura, String nome, double peso) {
-        this.altura = altura;
+    public Pessoa(String nome, double peso, double altura) {
         this.nome = nome;
         this.peso = peso;
+        this.altura = altura;
+        this.data = LocalDate.now();
     }
 
-    @Override
-    public String getNome() {
-       return this.nome;
-    }
+    public String getNome() { return nome; }
+    public double getPeso() { return peso; }
+    public double getAltura() { return altura; }
+    public LocalDate getData() { return data; }
 
-    @Override
-    public double getPeso() {
-      return this.peso;
-    }
+    public abstract double calcularIMC();
+    public abstract String classificarIMC();
+    public abstract String getTipo();
 
-    @Override
-    public double getAltura() {
-       return this.altura;
+    /**
+     * Formata os dados do registro para uma linha de arquivo CSV.
+     * @return String formatada como CSV.
+     */
+    public String toCSV() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return String.format("%s,%.2f,%.2f,%.2f,%s,%s\n",
+                nome, peso, altura, calcularIMC(), data.format(formatter), getTipo());
     }
-
 }
