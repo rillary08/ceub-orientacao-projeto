b80eea1ce2e3654e8bc07d6fa340f584a5e59634
VinnieMg <135880674+VinnieMg@users.noreply.github.com>
2025-06-24 20:18:12 -0300

    Delete Vinicius/projeto_final/calculadora_imc directory

--- Vinicius/projeto_final/calculadora_imc/src/model/RegistroIMC.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class RegistroIMC {
-
-    public RegistroIMC(String nome, double peso, double altura) {
-        super(nome, peso, altura)
-}
--- Vinicius/projeto_final/calculadora_imc/src/model/contract/IIMCController.java
+++
@@ -1,8 +0,0 @@
-package model.contract;
-
-import java.util.List;
-
-public interface IIMCController {
-    void adicionarRegistro(String nome, double peso, double altura);
-    List<String> getHistorico();
-}
