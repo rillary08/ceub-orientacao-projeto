f602aa9e9ac8331a34a98d5444a73ae7756e3ad9
rillary lorranne <rillary.lorranne@sempreceub.com>
2025-06-26 20:02:32 -0300

    Arrumando tudo para o projeto final

--- Calculadora_IMC/src/App.java
+++ Calculadora_IMC/src/App.java
@@ -1,5 +1,9 @@
+import pokemon.Pokemon;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Pokemon picachu = new Pokemon("Picachu");
+    
     }
 }
