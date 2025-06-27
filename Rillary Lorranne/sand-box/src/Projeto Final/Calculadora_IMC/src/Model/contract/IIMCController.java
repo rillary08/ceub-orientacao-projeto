ef0e7545e3b3011ee834a66a3d82614cf2025ea7
Rillary Lorranne <rillary.lorranne@sempreceub.com>
2025-06-26 22:47:37 -0300

    Update IIMCController.java
    Arrumando pelo git , já que meu vs code não está funcionando

--- Rillary Lorranne/sand-box/src/Projeto Final/Calculadora_IMC/src/Model/contract/IIMCController.java
+++ Rillary Lorranne/sand-box/src/Projeto Final/Calculadora_IMC/src/Model/contract/IIMCController.java
@@ -1,10 +1,29 @@
-package Model.contract;
+package model.contract;
 
 import java.util.List;
-
-import Model.RegistroIMC;
+import model.RegistroIMC;
 
 public interface IIMCController {
-    void adicionarRegistro(String nome, Double peso, Double altura);
-    List<RegistroIMC> getHistorico();  // vai ser lista de registro de imc
+    
+    // Registro de adultos
+    void adicionarRegistro(String nome, double peso, double altura);
+
+    // Registro de crianças
+    void adicionarRegistroInfantil(String nome, double peso, double altura, int idadeMeses);
+
+    // Histórico separado
+    List<RegistroIMC> getHistorico();
+    List<RegistroIMC> getHistoricoInfantil();
+
+    // Histórico completo (mistura adultos e infantis)
+    List<RegistroIMC> getHistoricoCompleto();
+
+    // Busca
+    List<RegistroIMC> buscarPorNome(String nome);
+
+    // Remoção
+    boolean removerRegistro(String nome);
+
+    // Exportação
+    boolean exportarHistoricoCSV(String caminhoArquivo); // true se sucesso
 }
