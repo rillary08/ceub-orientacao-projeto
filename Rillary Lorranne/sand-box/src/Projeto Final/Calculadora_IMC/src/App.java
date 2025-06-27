3a1da586bd37ce59db2fc006ccafa7968e615b85
Rillary Lorranne <rillary.lorranne@sempreceub.com>
2025-06-26 22:50:31 -0300

    Update App.java
    Arrumando por aqui , para nao me estressar com o vs code

--- Rillary Lorranne/sand-box/src/Projeto Final/Calculadora_IMC/src/App.java
+++ Rillary Lorranne/sand-box/src/Projeto Final/Calculadora_IMC/src/App.java
@@ -1,9 +1,16 @@
-import pokemon.Pokemon;
+package app;
+
+import javax.swing.SwingUtilities;
+import view.JanelaPrincipal;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-       
-        Pokemon picachu = new Pokemon("Picachu");
-    
+    public static void main(String[] args) {
+        System.out.println("Aplicação Calculadora de IMC iniciando...");
+
+        // A interface gráfica deve sempre rodar na Event Dispatch Thread (EDT)
+        SwingUtilities.invokeLater(() -> {
+            new JanelaPrincipal(); // Cria e exibe a janela
+            System.out.println("Interface carregada com sucesso.");
+        });
     }
 }
